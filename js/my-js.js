c9bae01b0d84c6e8c0f275df1a8aa73956b82e85
PrabeshRijal <53188731+PrabeshRijal@users.noreply.github.com>
2020-07-11 15:54:55 +0545

    fgr
    frf

--- js/my-js.js
+++ js/my-js.js
@@ -2,3 +2,39 @@ $(".custom-file-input").on("change", function() {
     var fileName = $(this).val().split("\\").pop();
     $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
   });
+// -------------------------
+function random()
+{
+  var a = document.getElementById('input').value;
+  if(a === 'science')
+  {
+    var array=['Day'];
+  }
+  else if(a === 'management')
+  {
+    var array=['Morning', 'Day'];
+  }
+  else
+  {
+    var array=[];
+  }
+
+  var string="";
+  for(i=0;i<array.length;i++)
+  {
+    string = string +'<option>' + array[i] + '</option>';
+  }
+  string = '<select name="">'+string+'</select>'
+  document.getElementById('output').innerHTML = string;
+}
+
+// --------------
+$(function() {
+  $("#input").change(function() {
+      if ($(this).val() == "management") {
+          $("#interested").prop("disabled", true);
+      }
+      else
+          $("#interested").prop("disabled", false);
+  });
+});
